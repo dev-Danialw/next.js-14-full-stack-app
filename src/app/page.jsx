58850075147c03ceaf341d39dc2dbaafc4307ce5
Danial-Waheed <daniyalwahied@gmail.com>
2024-01-19 14:52:35 +0500

    initial commit - with folder structure

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World! This is going to be a Full Stack Next.js app.
+    </main>
+  );
+}
